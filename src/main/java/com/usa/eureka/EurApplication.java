513a965dffcda2da28a07ed81ead85ae2941e3f9
Alex-CHO <hyungjin98@gmail.com>
2024-12-04 15:28:43 +0900

    eureka added

--- src/main/java/com/usa/eureka/EurApplication.java
+++ src/main/java/com/usa/eureka/EurApplication.java
@@ -2,12 +2,14 @@ package com.usa.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class EurekaApplication {
+public class EurApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication.run(EurApplication.class, args);
 	}
 
 }
